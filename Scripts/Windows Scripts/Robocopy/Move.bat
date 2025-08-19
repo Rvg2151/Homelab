138341b6d315be01fd5998d70a25156544658cc5
Robert Gonzales <RGonzalesCA@Aol.Com>
2025-08-18 19:55:13 -0700

    Update Move.bat

--- Scripts/Windows Scripts/Robocopy/Move.bat
+++ Scripts/Windows Scripts/Robocopy/Move.bat
@@ -1,5 +1,3 @@
-ROBOCOPY "D:\Mr Mainy Gamer\OUTPUT\DONE\MR MAINY GAMER" "V:\Youtube\Mr Mainy Gamer Youtube" /MOVE /E
-ROBOCOPY "D:\Mr Mainy Gamer - Twitch\OUTPUT\DONE" "V:\Youtube\Mr Mainy Gamer Twitch" /MOVE /E
 ROBOCOPY "D:\Music\OUTPUT\DONE" "V:\Youtube\Music" /MOVE /E
 ROBOCOPY "D:\Steam\OUTPUT\DONE" "V:\Youtube\Steam" /MOVE /E
 ROBOCOPY "D:\TD\OUTPUT\DONE" "V:\Youtube\To Download" /MOVE /E
@@ -12,3 +10,4 @@ ROBOCOPY "D:\ytdlp05\OUTPUT\DONE" "V:\Youtube\Youtube Channels" /MOVE /E
 ROBOCOPY "D:\ytdlp06\OUTPUT\DONE" "V:\Youtube\Youtube Channels" /MOVE /E
 ROBOCOPY "D:\ytdlp07\OUTPUT\DONE" "V:\Youtube\Youtube Channels" /MOVE /E
 ROBOCOPY "D:\ytdlp08\OUTPUT\DONE" "V:\Youtube\Youtube Channels" /MOVE /E
+
