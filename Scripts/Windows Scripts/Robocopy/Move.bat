805a890bf2860a274c8bcc7cda2307123a2293ce
Robert Gonzales <RGonzalesCA@Aol.Com>
2025-08-18 19:54:31 -0700

    Add files via upload

---
+++ Scripts/Windows Scripts/Robocopy/Move.bat
@@ -0,0 +1,14 @@
+ROBOCOPY "D:\Mr Mainy Gamer\OUTPUT\DONE\MR MAINY GAMER" "V:\Youtube\Mr Mainy Gamer Youtube" /MOVE /E
+ROBOCOPY "D:\Mr Mainy Gamer - Twitch\OUTPUT\DONE" "V:\Youtube\Mr Mainy Gamer Twitch" /MOVE /E
+ROBOCOPY "D:\Music\OUTPUT\DONE" "V:\Youtube\Music" /MOVE /E
+ROBOCOPY "D:\Steam\OUTPUT\DONE" "V:\Youtube\Steam" /MOVE /E
+ROBOCOPY "D:\TD\OUTPUT\DONE" "V:\Youtube\To Download" /MOVE /E
+ROBOCOPY "D:\WL\OUTPUT\DONE" "V:\Youtube\Watch Later" /MOVE /E
+ROBOCOPY "D:\ytdlp01\OUTPUT\DONE" "V:\Youtube\Youtube Channels" /MOVE /E
+ROBOCOPY "D:\ytdlp02\OUTPUT\DONE" "V:\Youtube\Youtube Channels" /MOVE /E
+ROBOCOPY "D:\ytdlp03\OUTPUT\DONE" "V:\Youtube\Youtube Channels" /MOVE /E
+ROBOCOPY "D:\ytdlp04\OUTPUT\DONE" "V:\Youtube\Youtube Channels" /MOVE /E
+ROBOCOPY "D:\ytdlp05\OUTPUT\DONE" "V:\Youtube\Youtube Channels" /MOVE /E
+ROBOCOPY "D:\ytdlp06\OUTPUT\DONE" "V:\Youtube\Youtube Channels" /MOVE /E
+ROBOCOPY "D:\ytdlp07\OUTPUT\DONE" "V:\Youtube\Youtube Channels" /MOVE /E
+ROBOCOPY "D:\ytdlp08\OUTPUT\DONE" "V:\Youtube\Youtube Channels" /MOVE /E
